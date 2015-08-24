4b3dd7dba06e23f128fbea0fb33832395b15311d
Richard Silverton <silverton.richard@googlemail.com>
2015-08-24 16:44:43 +0100

    corrected slide-ins package

--- packages/slide-ins/package.js
+++ packages/slide-ins/package.js
@@ -4,13 +4,14 @@ Package.describe({
   summary: 'Adds left and right slide-in menus',
   git: '',
   documentation: 'README.md'
-});
+})
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.1.0.3')
   api.use('templating@1.1.1', 'client')
   api.use('reactive-var@1.0.5', 'client')
   api.addFiles('slide-ins.html', 'client')
-  api.addFiles('slide-ins.js', 'client');
+  api.addFiles('slide-ins.js', 'client')
+  api.addFiles('slide-ins.css', 'client')
   api.export('SlideIns', 'client')
-});
+})

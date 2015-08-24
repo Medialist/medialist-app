5569d70ccc15c43672045504c5ad423112a5e759
Richard Silverton <silverton.richard@googlemail.com>
2015-08-24 16:33:01 +0100

    Adds custom slide-ins package

---
+++ packages/slide-ins/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'tableflip:slide-ins',
+  version: '0.0.1',
+  summary: 'Adds left and right slide-in menus',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.use('templating@1.1.1', 'client')
+  api.use('reactive-var@1.0.5', 'client')
+  api.addFiles('slide-ins.html', 'client')
+  api.addFiles('slide-ins.js', 'client');
+  api.export('SlideIns', 'client')
+});

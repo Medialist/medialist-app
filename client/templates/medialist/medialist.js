8a418892ec685f1aac29c9f725de1d0eaee2f97f
Richard Silverton <silverton.richard@googlemail.com>
2015-08-25 17:40:51 +0100

    added medialist template

--- client/templates/medialist/medialist.js
+++ client/templates/medialist/medialist.js
@@ -2,3 +2,9 @@ Template.medialist.onCreated(function () {
   this.slug = new ReactiveVar(FlowRouter.getParam('slug'))
   this.subscribe('medialist', this.slug.get())
 })
+
+Template.medialist.helpers({
+  medialist: function () {
+    return Medialists.findOne({slug: Template.instance().slug.get()})
+  }
+})

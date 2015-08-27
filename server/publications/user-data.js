ffe06e76f4fce2d5ecf6a9172e2ee71334229e9e
Oli Evans <oli@zilla.org.uk>
2015-08-27 14:56:47 +0100

    Tidy up user-data publish

--- server/publications/user-data.js
+++ server/publications/user-data.js
@@ -1,7 +1,8 @@
 Meteor.publish('userData', function () {
-  if (this.userId) {
-    return Meteor.users.find({_id: this.userId}, {fields: {groups: 1, 'services.twitter.profile_image_url_https': 1}});
-  } else {
-    this.ready();
-  }
-});
+  if (!this.userId) return this.ready()
+
+  return Meteor.users.find(
+    {_id: this.userId},
+    {fields: {'services.twitter.profile_image_url_https': 1}}
+  )
+})

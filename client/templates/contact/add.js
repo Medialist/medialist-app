21de35efd618c70c674520c917ce55c4b08953c1
Oli Evans <oli@zilla.org.uk>
2015-08-27 18:20:28 +0100

    Sharper colours, layout fixes & add contact form

--- client/templates/contact/add.js
+++ client/templates/contact/add.js
@@ -16,7 +16,7 @@ Template.addContact.helpers({
         $options: 'i'
       }
     }
-    return Contacts.find(query)
+    return Contacts.find(query, {limit:5})
   },
   name: function () {
     return name.get()

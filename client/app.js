42e30f31eb6d31cd87ab7aefe4d57f6b209e1d20
Richard Silverton <silverton.richard@googlemail.com>
2015-08-27 13:18:47 +0100

    removed unnecessary templateInstance.get method for the time being

--- client/app.js
+++ client/app.js
@@ -12,14 +12,3 @@ App.breadcrumbs = new ReactiveVar([
 App.cleanSBArgument = function (arg) {
   return (arg instanceof Spacebars.kw) ? undefined : arg
 }
-
-Blaze.TemplateInstance.prototype.get = function(key) {
-  var checkForKey = function(view) {
-    if (view && view.template instanceof Blaze.Template) {
-      var template = view.templateInstance()
-      if (typeof template[key] !== 'undefined') return template[key]
-    }
-    if (view.parentView) return checkForKey(view.parentView)
-  }
-  return checkForKey(this.view)
-}

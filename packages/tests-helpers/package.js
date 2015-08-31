5291a8c9b5791bb2b760ca1b2cedef4dd9c7b5c2
Richard Silverton <silverton.richard@googlemail.com>
2015-08-31 18:28:17 +0100

    rename test-helpers package

--- packages/tests-helpers/package.js
+++ packages/tests-helpers/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'test-login',
+  name: 'tests-helpers',
   version: '0.0.1',
   summary: 'Provide a method to log the user in without authentication (FOR TESTING PURPOSES ONLY)',
   documentation: 'README.md',
@@ -9,6 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3')
   api.use('accounts-base@1.2.0')
-  api.addFiles('test-login.js', ['client', 'server'])
+  api.addFiles('tests-helpers.js', ['client', 'server'])
   api.export('testLogin', 'client')
 })

c2ea55e38082ecb63430f51856c3426938abfa81
Richard Silverton <silverton.richard@googlemail.com>
2015-09-01 11:32:12 +0100

    adds checks to posts publication

--- server/publications/posts.js
+++ server/publications/posts.js
@@ -1,6 +1,11 @@
 Meteor.publish('posts', function (medialistSlug, contactSlug, limit, message) {
-  if (!this.userId) return this.ready()
+  check(medialistSlug, String)
+  check(contactSlug, String)
+  if (!message) message = false
+  check(message, Boolean)
   if (!limit) limit = 1
+  check(limit, Number)
+  if (!this.userId) return this.ready()
   var query = {}
   if (medialistSlug) query.medialists = medialistSlug
   if (contactSlug) query.contacts = contactSlug

b986290b959b1fc499ebf3ec41c8e0bb54521463
Oli Evans <oli@zilla.org.uk>
2015-09-09 17:30:51 +0100

    Adds all contacts page
    - /contacts route
    - all contacts, limit 100, pagination a TODO.
    - Updates contact publish and create to make a medialist optional
    - Adds links to medialists contact is involved with.

--- server/publications/contacts.js
+++ server/publications/contacts.js
@@ -1,16 +1,28 @@
-Meteor.publish('contacts', function (string) {
-  if (typeof string !== 'string') string = ''
-  var regex = new RegExp(string, 'gi')
-  var query = {
-    name: {
-      $regex: regex,
-      $options: 'i'
-    }
+Meteor.publish('contacts', function (opts) {
+  if (!this.userId) return this.ready()
+
+  opts = opts || {}
+  check(opts, {
+    name: Match.Optional(String),
+    limit: Match.Optional(Number)
+  })
+
+  var query = {}
+  if (opts.name) {
+    var regex = new RegExp(opts.name, 'gi')
+    query.name = { $regex: regex, $options: 'i'}
   }
-  return Contacts.find(query, { limit: App.contactSuggestions })
+
+  var options = {
+    sort: { createdAt: -1 },
+    limit: opts.limit || App.contactSuggestions
+  }
+
+  return Contacts.find(query, options)
 })
 
 Meteor.publish('contact', function (slug) {
+  if (!this.userId) return this.ready()
   check(slug, String)
   return Contacts.find({slug: slug})
 })

d1b6a6bb7a97109d555ca99b983794d4562ffb7e
Richard Silverton <silverton.richard@googlemail.com>
2015-09-23 10:27:13 +0100

    Adds filter to Contacts page

--- client/templates/contacts/contacts.js
+++ client/templates/contacts/contacts.js
@@ -1,15 +1,33 @@
 Template.contacts.onCreated(function () {
+  this.filterTerm = new ReactiveVar()
   this.subscribe('contacts', {limit: 100})
 })
 
 Template.contacts.helpers({
   allContacts: function () {
-    return Contacts.find({}, {sort: {'roles.[0].org': 1}})
+    var filterTerm = Template.instance().filterTerm.get()
+    var query = {}
+    if (filterTerm) {
+      var filterRegExp = new RegExp(filterTerm, 'gi')
+      query.$or = [
+        { 'name': filterRegExp },
+        { 'roles.0.title': filterRegExp },
+        { 'roles.0.org': filterRegExp }
+      ]
+    }
+    return Contacts.find(query, {sort: {'roles.[0].org': 1}})
+  },
+  filterTerm: function () {
+    return Template.instance().filterTerm.get()
   }
 })
 
 Template.contacts.events({
   'click [data-action="add-new"]': function () {
     Modal.show('addContact')
+  },
+  'keyup [data-field="filter-term"]': function (evt, tpl) {
+    var filterTerm = tpl.$(evt.currentTarget).val()
+    Template.instance().filterTerm.set(filterTerm)
   }
 })

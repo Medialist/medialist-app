61018f62e5e1b537871ac126f3704343eec10516
Richard Silverton <silverton.richard@googlemail.com>
2015-09-23 10:39:01 +0100

    Adds filter to All Medialists page

--- client/templates/medialists/medialists.js
+++ client/templates/medialists/medialists.js
@@ -1,15 +1,32 @@
 Template.medialists.onCreated(function () {
+  this.filterTerm = new ReactiveVar()
   this.subscribe('medialists')
 })
 
 Template.medialists.helpers({
   medialists: function () {
-    return Medialists.find({}, {sort: {updatedAt: -1}}).fetch()
+    var filterTerm = Template.instance().filterTerm.get()
+    var query = {}
+    if (filterTerm) {
+      var filterRegExp = new RegExp(filterTerm, 'gi')
+      query.$or = [
+        { 'name': filterRegExp },
+        { 'purpose': filterRegExp }
+      ]
+    }
+    return Medialists.find(query, {sort: {updatedAt: -1}}).fetch()
+  },
+  filterTerm: function () {
+    return Template.instance().filterTerm.get()
   }
 })
 
 Template.medialists.events({
   'click [data-action="create-medialist"]': function () {
     Modal.show('createMedialist')
+  },
+  'keyup [data-field="filter-term"]': function (evt, tpl) {
+    var filterTerm = tpl.$(evt.currentTarget).val()
+    Template.instance().filterTerm.set(filterTerm)
   }
 })

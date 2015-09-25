6044f7947532f179afb0c0ca5514e2839e68b4e2
Oli Evans <oli@zilla.org.uk>
2015-09-25 17:28:48 +0100

    Fix jshint and add user check to server methods

--- server/methods/clients.js
+++ server/methods/clients.js
@@ -1,5 +1,6 @@
 Meteor.methods({
   'clients/search': function (term) {
+    if (!this.userId) throw new Meteor.Error('Only a logged-in user can search')
     if (!term) return []
     var regExp = new RegExp(term, 'i')
     return Clients.find( { name: regExp }, { limit: App.clientSuggestions } ).map( client => client.name )

9a8e55392beb351ac2c448c044c40bfa7ece63a3
Richard Silverton <silverton.richard@googlemail.com>
2015-09-30 14:42:39 +0100

    Adds a migration method for contact roles

---
+++ server/methods/migrations.js
@@ -0,0 +1,26 @@
+Meteor.methods({
+  'migrations/contacts/0-1': function () {
+    checkMigrator(this.userId)
+    Contacts.find().forEach(contact => {
+      _.forEach(contact.roles, role => {
+        if (typeof role.org !== 'string') return
+        var orgName = role.org
+        role.org = { name: orgName }
+        var org = Orgs.findOne({ name: role.org.name })
+        if (org) {
+         role.org._id = org._id
+        } else {
+         role.org._id = Orgs.insert({ name: role.org.name })
+        }
+      })
+      Contacts.update(contact._id, contact)
+    })
+  }
+})
+
+function checkMigrator(userId) {
+  if (!userId) throw new Meteor.Error('Only a logged-in user can run migrations')
+  var user = Meteor.users.findOne(userId)
+  var whitelist = Meteor.settings && Meteor.settings.migrations.whitelist
+  if (whitelist.indexOf(user.services.twitter.screenName) === -1) throw new Meteor.Error('Only a whitelisted user can run migrations')
+}

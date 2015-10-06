21ac3879bd2b920b4383ba2c715403e1a54ec061
Richard Silverton <silverton.richard@googlemail.com>
2015-10-06 11:15:13 +0100

    un-prettify helpers.js

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,13 +1,13 @@
 var helpers = {
-  getPath: function(route, params, queryParams) {
+  getPath: function (route, params, queryParams) {
     params = App.cleanSBArgument(params)
     queryParams = App.cleanSBArgument(queryParams)
     return FlowRouter.path(route, params, queryParams)
   },
-  equal: function(x, y) {
+  equal: function (x, y) {
     return x === y
   },
-  getMedialists: function(contact) {
+  getMedialists: function (contact) {
     contact = contact || this
     var medialistSlugs = contact.medialists
     return Medialists.find({
@@ -16,37 +16,37 @@ var helpers = {
       }
     })
   },
-  statuses: function() {
+  statuses: function () {
     return _.values(Contacts.status)
   },
-  classify: function(string) {
+  classify: function (string) {
     return s.slugify(string)
   },
-  fromNow: function(date) {
+  fromNow: function (date) {
     return date.fromNow()
   },
   instance: function() {
     return Template.instance()
   },
-  routeSlug: function() {
+  routeSlug: function () {
     FlowRouter.watchPathChange()
     return FlowRouter.getParam('slug')
   },
-  firstName: function(fullName) {
+  firstName: function (fullName) {
     var ind = fullName.indexOf(' ')
     return ind > -1 ? fullName.substr(0, ind) : fullName
   },
-  profileImage: function() {
+  profileImage: function () {
     var user = Meteor.user();
     if (!user || !user.services) return null;
     return user.services.twitter.profile_image_url_https;
   },
-  youOrName: function(user) {
+  youOrName: function (user) {
     if (user && user._id === Meteor.userId()) return 'You'
     return user && user.name
   }
 }
 
-_.each(helpers, function(helper, name) {
+_.each(helpers, function (helper, name) {
   Template.registerHelper(name, helper)
 })

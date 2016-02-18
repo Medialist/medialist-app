108aecbbb549564250d94a325121bea610e5a091
Richard Silverton <silverton.richard@googlemail.com>
2016-02-18 10:06:11 +0000

    Adds methods for creating fake data to test package

--- packages/tests-helpers/package.js
+++ packages/tests-helpers/package.js
@@ -4,11 +4,16 @@ Package.describe({
   summary: 'Provide a method to log the user in without authentication (FOR TESTING PURPOSES ONLY)',
   documentation: 'README.md',
   debugOnly: true
-});
+})
+
+Npm.depends({
+  faker: '3.0.1'
+})
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3')
   api.use('accounts-base@1.2.0')
+  api.use('ecmascript@0.1.6')
   api.addFiles('tests-helpers.js', ['client', 'server'])
   api.export('testLogin', 'client')
 })

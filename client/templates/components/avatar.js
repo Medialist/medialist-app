321587a694940166d820757959b7541b3e087da4
Oli Evans <oli@zilla.org.uk>
2016-02-18 12:09:55 +0000

    Use a regular event map; stay reactive

--- client/templates/components/avatar.js
+++ client/templates/components/avatar.js
@@ -1,12 +1,12 @@
-Template.avatar.onRendered(function () {
-  if (!this.data.avatar || !this.data.socials) return
-  var twitterId = this.data.socials.reduce((res, social) => {
-    // TODO: migrate id to twitterId https://github.com/Medialist/medialist-app/issues/253
-    return social.id || res
-  }, null)
-  if (!twitterId) return
-  var img = new Image(40, 40)
-  img.onload = () => { this.firstNode.src = img.src }
-  img.onerror = () => { Meteor.call('twitter/updateAvatar', twitterId) }
-  img.src = this.data.avatar
+Template.avatar.events({
+  'error img': function (evt, tpl) {
+    $(evt.currentTarget).attr('src', '/images/avatar.svg')
+    if (!tpl.data.socials) return /* no socials */
+    var twitterId = tpl.data.socials.reduce((res, social) => {
+      // TODO: migrate id to twitterId https://github.com/Medialist/medialist-app/issues/253
+      return social.id || res
+    }, null)
+    if (!twitterId) return
+    Meteor.call('twitter/updateAvatar', twitterId)
+  }
 })
